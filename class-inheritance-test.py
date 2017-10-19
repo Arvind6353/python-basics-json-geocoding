0259e41cd7100b1727b879ff28b167d7ffda7169
T A <arvta@paypalcorp.com>
2017-10-19 13:15:22 +0530

    oop concepts

--- class-inheritance-test.py
+++ class-inheritance-test.py
@@ -1,3 +1,4 @@
+# parent class
 class Person :
 
     def __init__(self, age, name):
@@ -17,6 +18,7 @@ class Person :
         self.__age = val
 
 
+# sub class 
 class Student(Person) :
     
     def __init__(self, age , name , id) :
@@ -35,8 +37,8 @@ class Student(Person) :
     def __str__(self) :
         return ' the name is ' + self.GetName() +' -- the age is '+ str(self.GetAge()) + ' -- id is '+ str(self.GetId())
 
-s = Student(22,'arvind',0)
-# the name is arvind -- the age is 22 -- id is 0
+s = Student(22,'arvind',11)
+# the name is arvind -- the age is 22 -- id is 11
         
 print(s)
 
